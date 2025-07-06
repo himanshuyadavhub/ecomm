d20dcea6a8881122675eb5f18f8dd86e41871bd2
Himanshu <himanshuyadav9536w@gmail.com>
2025-07-06 14:15:51 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+
+app.get("/products",(req,res)=>{
+    res.send("<h1>Here is the list of all Products</h1>")
+});
+
+app.post("/products",(req,res)=>{
+    res.send("<h1>A new product has been added</h1>");
+})
+
+app.get("/categories",(req,res)=>{
+    res.send("<h1>Here is the list of all Categories.</h1>")
+});
+
+app.post("/categories",(req,res)=>{
+    res.send("<h1>A new Category has been created.</h1>");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+})
